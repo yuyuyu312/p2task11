4e306d8eaaf72c1e140f9e8cb8a444c6b180bb36
yuyuyu312 <yusupov.ilgiz2323@gmail.com>
2019-06-25 18:56:06 +0600

    p2task11

---
+++ p2task11.py
@@ -0,0 +1,13 @@
+a = "strongstring"
+print(a[2])
+print(a[2])
+print(a[:5])
+print(a[-2:])
+for i in range(0, len(a), 2):
+    print(a[i])
+for i in range(1, len(a), 3):
+    print(a[i])
+print(a[::-1])
+for i in range(len(a), -1, -1):
+    print(a[i])
+print(len(a))
